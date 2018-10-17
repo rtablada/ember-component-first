170f0de4dbdf2620e7bbab267b4ab01840d341c9
Ryan Tablada <ryan.tablada@gmail.com>
2018-10-16 22:15:47 -0500

    wip: request data from server

--- game-list/app/components/game-list.js
+++ game-list/app/components/game-list.js
@@ -12,21 +12,19 @@ export default class extends Component {
 
   async loadData() {
     this.set('loading', true);
-    await timeout(1000);
+
+    const req = await fetch('http://localhost:3000/games', {
+      credentials: 'include'
+    });
+    const games = await req.json();
+
     this.set('loading', false);
-    this.set('games', [
-      {
-        title: "Overwatch",
-        year: "2016",
-        imageUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/fen88hu0vhcf3k3owkxd.jpg",
-        description: "A good first person shooter",
-      },
-      {
-        title: "Mario Party Ultimate",
-        year: "2018",
-        imageUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/rvadchpkfknklxmckmmk.jpg",
-        description: "A great way to compete with friends!",
-      }
-    ]);
+    this.set('games', games.map(g => ({
+      title: g.name,
+      rating: g.esrb.rating,
+      imageUrl: g.cover.url,
+      description: g.summary,
+      id: g.id
+    })));
   }
 }
--- server/main.js
+++ server/main.js
@@ -1,74 +1,86 @@
-import Koa from 'koa';
-import logger from 'koa-logger';
-import KoaRouter from 'koa-router';
-import bodyParser from 'koa-bodyparser';
-import cors from '@koa/cors';
-import fetch from 'node-fetch';
-import _ from 'lodash';
-import session from 'koa-session';
+import Koa from "koa";
+import logger from "koa-logger";
+import KoaRouter from "koa-router";
+import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
+import fetch from "node-fetch";
+import _ from "lodash";
+import session from "koa-session";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-
 
 const config = {
   apiKey: process.env.IGDB_USER_KEY,
   port: process.env.PORT || 3000,
-  secretKey: 'SOMETHINGSUPERSECRET'
+  secretKey: "SOMETHINGSUPERSECRET"
 };
 
-const recentGamesUrl = () => {
-  const time = (new Date()).getTime();
+const replaceImages = game => ({
+  ...game,
+  cover: {
+    ...game.cover,
+    url: `//images.igdb.com/igdb/image/upload/t_cover_big/${
+      game.cover.cloudinary_id
+    }.jpg`
+  }
+});
 
-  return `games/?fields=name,first_release_date,cover,esrb,summary` +
+const recentGamesUrl = () => {
+  const time = new Date().getTime();
+
+  return (
+    `games/?fields=name,first_release_date,cover,esrb,summary` +
     `&order=popularity:desc` +
-    `&filter[created_at][not_eq]=1511784798211&filter[esrb][exists]=true&limit=20`;
-}
+    `&filter[created_at][not_eq]=1511784798211&filter[esrb][exists]=true&limit=20`
+  );
+};
 
 const getPopularGames = async () => {
-  const result = await fetch(`https://api-endpoint.igdb.com/${recentGamesUrl()}`, {
-    headers: {
-      "user-key": config.apiKey,
-      Accept: "application/json"
+  const result = await fetch(
+    `https://api-endpoint.igdb.com/${recentGamesUrl()}`,
+    {
+      headers: {
+        "user-key": config.apiKey,
+        Accept: "application/json"
+      }
     }
-  });
+  );
 
   return await result.json();
-}
+};
 
 const app = new Koa();
 
 app.keys = [config.secretKey];
 
 app.use(logger());
-app.use(cors());
+app.use(cors({credentials: true}));
 app.use(bodyParser());
 app.use(session(app));
 
 var router = new KoaRouter();
 
-router.get('/games', async (ctx) => {
-  const cookieGames = ctx.cookies.get('games');
-
+router.get("/games", async ctx => {
   try {
     if (ctx.session.games) {
-      return ctx.body = ctx.session.games;
+      ctx.body = ctx.session.games.map(a => replaceImages(a));
+    } else {
+      const popularGames = await getPopularGames();
+      const selectedGames = _.chain(popularGames)
+        .shuffle()
+        .take(2)
+        .value();
+
+      ctx.session.games = selectedGames;
+      ctx.body = selectedGames.map(a => replaceImages(a));
     }
-
-    const popularGames = await getPopularGames();
-    const selectedGames = _.chain(popularGames).shuffle().take(2).value();
-
-    ctx.body = selectedGames;
-
-    ctx.session.games = selectedGames;
   } catch (error) {
-    debugger;
     ctx.status = 500;
 
-    ctx.body = {message: error.message};
+    ctx.body = { message: error.message };
   }
 });
-
 
 app.use(router.allowedMethods());
 app.use(router.routes());
